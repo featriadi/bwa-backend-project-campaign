2c77c287b218f97fc0d2d2059ee8eff9d768647e
featriadi <aditya.fitriadi99@gmail.com>
2021-04-13 23:12:00 +0700

    Register User

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"bwastartup/user"
 	"log"
 
 	"gorm.io/driver/mysql"
@@ -10,11 +10,20 @@ import (
 
 func main() {
 	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Successfully connected to database")
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+
+	userInput := user.RegisterUserInput{}
+	userInput.Name = "Agus"
+	userInput.Occupation = "Musisi"
+	userInput.Email = "agus@gmail.com"
+	userInput.Password = "master123"
+
+	userService.RegisterUser(userInput)
 }
