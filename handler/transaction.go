a489c97037a2ae555775a852decd0fee72afe47e
featriadi <aditya.fitriadi99@gmail.com>
2021-04-24 00:35:40 +0700

    get campaign transaction

---
+++ handler/transaction.go
@@ -0,0 +1,42 @@
+package handler
+
+import (
+	"bwastartup/helper"
+	"bwastartup/transaction"
+	"bwastartup/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type transactionHandler struct {
+	service transaction.Service
+}
+
+func NewTransactionHandler(service transaction.Service) *transactionHandler {
+	return &transactionHandler{service}
+}
+
+func (h *transactionHandler) GetCampaignTransactions(c *gin.Context) {
+	var input transaction.GetCampaignTransactionInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	currectUser := c.MustGet("currentUser").(user.User)
+	input.User = currectUser
+
+	transactions, err := h.service.GetTransactionByCampaignID(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Campaign transactions", http.StatusOK, "success", transaction.FormatCampaignTransactions(transactions))
+	c.JSON(http.StatusOK, response)
+}
