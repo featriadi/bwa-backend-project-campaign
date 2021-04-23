6f1e138facb4fccec877e92e5ae5685146e6ba35
featriadi <aditya.fitriadi99@gmail.com>
2021-04-23 16:19:05 +0700

    create campaign images

--- campaign/repository.go
+++ campaign/repository.go
@@ -8,6 +8,8 @@ type Repository interface {
 	FindByID(ID int) (Campaign, error)
 	Save(campaign Campaign) (Campaign, error)
 	Update(campaign Campaign) (Campaign, error)
+	CreateCampaignImage(campaignImage CampaignImage) (CampaignImage, error)
+	SetAllImageToNonPrimary(campaignID int) (bool, error)
 }
 
 type repository struct {
@@ -68,4 +70,23 @@ func (r *repository) Update(campaign Campaign) (Campaign, error) {
 	}
 
 	return campaign, nil
+}
+
+func (r *repository) CreateCampaignImage(campaignImage CampaignImage) (CampaignImage, error) {
+	err := r.db.Create(&campaignImage).Error
+	if err != nil {
+		return campaignImage, err
+	}
+
+	return campaignImage, nil
+}
+
+func (r *repository) SetAllImageToNonPrimary(campaignID int) (bool, error) {
+	err := r.db.Model(&CampaignImage{}).Where("campaign_id = ?", campaignID).Update("is_primary", false).Error
+
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
